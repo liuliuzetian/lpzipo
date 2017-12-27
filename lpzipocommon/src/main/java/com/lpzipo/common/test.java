d9eb6823d9fed710a819522a0fa6cf0206eceabf
liuzetian <wb.liuzetian@mesg.corp.netease.com>
2017-12-27 20:11:03 +0800

    project begin

---
+++ lpzipocommon/src/main/java/com/lpzipo/common/test.java
@@ -0,0 +1,19 @@
+package com.lpzipo.common;
+
+import java.util.Calendar;
+
+public class test {
+
+    /**
+     * idea main 快捷键是 psvm
+     *
+     * idea System.out.println快捷键sout tab
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+
+    }
+}
